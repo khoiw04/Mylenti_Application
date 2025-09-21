af447d3b5dfd9b15c1800aa34c568c33e4fd1ef7
khoiw04 <khoi.w04@gmail.com>
2025-09-21 17:32:47 +0700

    Cải thiện nút Dừng, Lỗi của YouTube

--- src/components/pages/index/ResumePolling.tsx
+++ src/components/pages/index/ResumePolling.tsx
@@ -1,15 +1,15 @@
-import { LucidePlay } from "lucide-react";
+import { LucideBug, LucidePlay } from "lucide-react";
 import { useStore } from "@tanstack/react-store";
 import ButtonFunction from './ButtonFunction'
 import { PollingStatusStore } from "@/store";
 
 export default function ResumePolling() {
-    const { manualPolling } = useStore(PollingStatusStore)
+    const { manualPolling, isError } = useStore(PollingStatusStore)
     
     return (
         <ButtonFunction
-            icon={<LucidePlay />}
-            description="Lỗi hoặc Dừng? Nếu Dừng, 30 giây sau sẽ tự khởi động. Nếu lỗi! Cần khởi động lại."
+            icon={isError ? <LucideBug /> : <LucidePlay />}
+            description={isError ? "Lỗi! Cần khởi động lại." : "30 giây sau sẽ tự khởi động"}
             title="Khởi Động lại Khung Chat"
             onClick={manualPolling}
         />
